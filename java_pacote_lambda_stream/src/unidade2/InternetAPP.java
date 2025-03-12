145cffa4825e21aba35c37945c22931deda0bc69
Eduard Teixeira <eduardteixeira01@gmail.com>
2025-03-12 16:36:12 -0300

    Anotações

---
+++ java_pacote_lambda_stream/src/unidade2/InternetAPP.java
@@ -0,0 +1,19 @@
+package unidade2;
+
+@Copyright(autor = "Eduard", data = "12/03/2025", versao = "1.0")
+public class InternetAPP implements Internet {
+
+	public static void main(String[] args) {
+
+	}
+
+	@Deprecated
+	public void conectar() {
+	}
+
+	@Override
+	public void conectarSSL() {
+		// TODO Auto-generated method stub
+	}
+
+}
